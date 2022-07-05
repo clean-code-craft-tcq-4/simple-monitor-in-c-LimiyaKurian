4e02dcc0f748b5d07483ffc0dfdb2ce389cc0d64
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-07-05 21:17:13 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -45,8 +45,8 @@ return result;
 int main() {
   assert(Checktemperature(25));
   assert(checkSoc(70));
-  assert(checkChargerate(0.9));
-  assert(batteryIsOk(25, 70, 0.9));
+  assert(checkChargerate(0.7));
+  assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
     
