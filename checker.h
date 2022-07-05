4e6f30c8ae5f1ae44a66c8c52116bd958dc7ce6b
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-07-06 01:27:08 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -1,7 +1,14 @@
 
-#define E_NOT_OK 0
-#define E_OK     1
+#define E_NOT_OK      0
+#define E_OK          1
+#define TEMP_MIN      0
+#define TEMP_MAX      45
+#define SOC_MIN       20
+#define SOC_MAX       80
+#define CHARGE_LIMIT  0.8
 
 int Checktemperature(float temperature);
 int checkSoc(float soc);
 int checkChargerate(float chargeRate);
+int checkparametermargin(float param,float min_margin , float max_margin);
+int checkparalimit(float param, float limit);
