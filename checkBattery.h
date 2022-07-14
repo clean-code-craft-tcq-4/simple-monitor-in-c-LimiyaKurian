131eb9bf7c06f95b3f526b2c8643a9d4f1cee60b
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-07-15 00:48:32 +0530

    Update checkBattery.h

--- checkBattery.h
+++ checkBattery.h
@@ -7,9 +7,18 @@
 #define SOC_MAX       80
 #define CHARGE_LIMIT  0.8
 
+//#define TEMPERATURE_IN_FARENHEIT
+
+#ifdef TEMPERATURE_IN_FARENHEIT
+#define GET_TEMPERATURE_IN_CELCIUS(parameter) convertFarenheitToCelcius(parameter)
+#else
+#define GET_TEMPERATURE_IN_CELCIUS(parameter) (parameter)
+#endif
+
 int Checktemperature(float temperature);
 int checkSoc(float soc);
 int checkChargerate(float chargeRate);
 int checkParameterMargin(float param,float min_margin , float max_margin);
 int checkParameterLimit(float param, float limit);
 void IsReachingWarningLevel(char *param,float value,float min_margin , float max_margin);
+int Checktemperature(float temperature);
